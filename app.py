ddb6275c4e62bf87100e1dd81f1f6aa68d04f9cd
fajarisfan <102415771+fajarisfan@users.noreply.github.com>
2026-05-09 06:25:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import pandas as pd
 import streamlit as st
 import tabula
 import pdfplumber
-import plotly.graph_objects as go
 from datetime import datetime, timezone, timedelta
 
 # ── CONFIG ──────────────────────────────────────────────────
